045c0c9bf4a7ba64c051bf66a1e34e16e88f6a63
Valter Fernandes Filho <valterfernandes.dev@gmail.com>
2021-07-20 19:25:07 -0300

    configure user session with 30 minutes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_user
+
+  private
+
+  def set_user    
+    session[:current_user_id] = session[:current_user_id].presence || SecureRandom.uuid
+  end
 end
