6b4673d8c6d965c9902b89dde0b1791fd12678ad
Valter Fernandes Filho <valterfernandes.dev@gmail.com>
2021-07-21 20:23:27 -0300

    create errors controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,11 @@
 class ErrorsController < ApplicationController
   protect_from_forgery with: :null_session
 
-  def not_found   
-    render(status: 404)
+  def not_found
+    render(status: :not_found)
   end
 
   def internal_server_error
-    render(status: 500)
+    render(status: :internal_server_error)
   end
 end
