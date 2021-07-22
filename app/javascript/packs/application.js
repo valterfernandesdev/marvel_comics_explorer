4162622f668dcc8f601a8bf137757e1c0ec7c1e4
Valter Fernandes Filho <valterfernandes.dev@gmail.com>
2021-07-21 22:02:53 -0300

    extract back to top btn js to a separated file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,31 +11,3 @@ import 'bootstrap/dist/js/bootstrap'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-jQuery(function() {
-  //Get the button
-  let mybutton = document.getElementById("btn-back-to-top");
-
-  // When the user scrolls down 80px from the top of the document, show the button
-  window.onscroll = function () {
-    scrollFunction();
-  };
-
-  function scrollFunction() {
-    if (
-      document.body.scrollTop > 500 ||
-      document.documentElement.scrollTop > 500
-    ) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
-
-  function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
-  // When the user clicks on the button, scroll to the top of the document
-  mybutton.addEventListener("click", backToTop);
-});
